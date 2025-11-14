e7b9c41c1eeaf03e6a65e11986e45662a8b20f3a
baseganni <ashwiniu.23.becs@acharya.ac.in>
2025-11-14 13:36:23 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,10 @@
+SELECT 
+    customers.name AS customer_name,
+    products.name AS product_name,
+    order_items.quantity,
+    orders.order_date
+FROM customers
+JOIN orders ON customers.id = orders.customer_id
+JOIN order_items ON orders.order_id = order_items.order_id
+JOIN products ON order_items.product_id = products.id;
 
